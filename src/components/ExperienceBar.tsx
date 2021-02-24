dc1ce0fc1581b2f949c5b2f67f58a178c832b719
Kayro França <k4yro01@gmail.com>
2021-02-24 18:48:25 -0300

    add new style component

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,10 +1,12 @@
+import styles from "../styles/components/ExperienceBar.module.css";
+
 export function ExperienceBar() {
     return (
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
                 <div style={{ width: '10%' }} />
-                <span className="current-xp" style={{ left:'10%' }}> 60 xp</span>
+                <span className={styles.currentXp} style={{ left:'10%' }}> 60 xp</span>
             </div>
             <span>600 xp</span>
         </header>
