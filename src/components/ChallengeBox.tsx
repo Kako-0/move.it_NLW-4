a53f280c57c018d250a06fbb98175bd50760eed8
Kayro França <k4yro01@gmail.com>
2021-02-27 13:32:38 -0300

    uptade state with context

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -3,18 +3,17 @@ import { ChallengesContext } from "../contexts/ChallengesContexts";
 import styles from "../styles/components/ChallengeBox.module.css";
 
 export function ChallengeBox() {
-    const contextData = useContext(ChallengesContext);
-    const hasActiveChallenge = true;
+    const { activeChallenge } = useContext(ChallengesContext);
 
     return (
         <div className={styles.challengeBoxContainer}>
-            {hasActiveChallenge ? ( 
+            {activeChallenge ? ( 
                 <div className={styles.challengeBoxActive}>
-                    <header>Ganhe 400 de xp</header>
+                    <header>Ganhe {activeChallenge.amount} de xp</header>
                     <main>
-                        <img src="icons/body.svg" alt="body"/>
+                        <img src={`icons/${activeChallenge.type}.svg`} alt="body"/>
                         <strong>Novo Desafio</strong>
-                        <p>Levante e faça uma caminhada de 3 minutos.</p>
+                        <p>{activeChallenge.description}</p>
                     </main>
                     <footer>
                         <button type="button" className={styles.challengeFailedButton}>
