4a4c80ea98576826bd99a06278db7db3e0a4c439
Kayro França <k4yro01@gmail.com>
2021-02-27 13:30:20 -0300

    new state activeChalllenge

--- src/contexts/ChallengesContexts.tsx
+++ src/contexts/ChallengesContexts.tsx
@@ -1,11 +1,19 @@
 import { createContext, ReactNode, useState } from "react";
 
+import challenges from "../../challenges.json";
+
+interface Challenge {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
 interface ChallengeContextData {
     level: number;
     currenceXP: number; 
     challengeCompleted: number; 
     levelUp: () => void;
     startNewChallenge: () => void;
+    activeChallenge: Challenge;
 }
 interface ChallengerProviderProps {
     children : ReactNode;
@@ -17,13 +25,17 @@ export function ChallengesProvider({ children }: ChallengerProviderProps) {
     const [level, setLevel] = useState(1);
     const [currenceXP, setCurrenceXP] = useState(0);
     const [challengeCompleted, setChallengeCompleted] = useState(0);
+    const [activeChallenge, setActiveChallenge] = useState(null);
     
     function levelUp() {
         setLevel(level + 1);
     }
     
     function startNewChallenge() {
-        console.log('New challenge')    
+        const randomChallengeindex = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallengeindex];  
+
+        setActiveChallenge(challenge);
     }
 
     return(
@@ -33,7 +45,8 @@ export function ChallengesProvider({ children }: ChallengerProviderProps) {
                 currenceXP, 
                 challengeCompleted, 
                 levelUp,
-                startNewChallenge
+                startNewChallenge,
+                activeChallenge
             }}
         >
             {children}
