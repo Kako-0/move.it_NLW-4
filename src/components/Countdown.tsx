0c3563d20a5d67d31c3cce8bf415564838cdaa6c
Kayro França <k4yro01@gmail.com>
2021-02-27 12:32:25 -0300

    use the feature context on components

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContexts";
 import styles from "../styles/components/Countdown.module.css";
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
+
+    const { startNewChallenge } = useContext(ChallengesContext);
+
     const [isActive, setIsActive] = useState(false);
     const [time, setTime] = useState(0.05 * 60);
     const [hasFinished, setHasFinished] = useState(false);
@@ -32,6 +36,7 @@ export function Countdown() {
         } else if (isActive && time === 0) {
             setHasFinished(true);
             setIsActive(false);
+            startNewChallenge();
         }
     }, [isActive, time]);
 
